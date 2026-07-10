70b2fff1d3001000209a432fbb1e1f7e63b16260
ibrahimabdulsalam2010ab-commits <ibrahimabdulsalam2010ab@gmail.com>
2026-07-10 01:42:06 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,32 @@
-
+const questions = [
+  {
+    question: "What is the capital of Nigeria?",
+    options: ["Lagos", "Abuja", "Kano", "Ibadan"],
+    answer: "Abuja"
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    options: ["Earth", "Venus", "Mars", "Jupiter"],
+    answer: "Mars"
+  },
+  {
+    question: "5 × 8 = ?",
+    options: ["35", "40", "45", "50"],
+    answer: "40"
+  },
+  {
+    question: "Water freezes at?",
+    options: ["0°C", "50°C", "100°C", "25°C"],
+    answer: "0°C"
+  },
+  {
+    question: "HTML stands for?",
+    options: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "High Text Machine Language",
+      "Hyper Tool Machine Language"
+    ],
+    answer: "Hyper Text Markup Language"
+  }
+];
