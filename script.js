0a92c2e1e3b4d2667a310ad77f8164577fbb1991
ibrahimabdulsalam2010ab-commits <ibrahimabdulsalam2010ab@gmail.com>
2026-07-10 01:43:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-document.querySelector('button').onclick=()=>alert('Quiz engine coming next!');
+const startBtn = document.getElementById("startBtn");
+const nameInput = document.getElementById("playerName");
+
+if (startBtn) {
+  startBtn.addEventListener("click", () => {
+    const name = nameInput.value.trim();
+
+    if (name === "") {
+      alert("Please enter your name.");
+      return;
+    }
+
+    localStorage.setItem("playerName", name);
+    window.location.href = "quiz.html";
+  });
+}document.querySelector('button').onclick=()=>alert('Quiz engine coming next!');
